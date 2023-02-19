117b69891c8f51727ed4da3d75d853b35e7c370e
ghPedroBM <31487343+ghPedroBM@users.noreply.github.com>
2023-02-19 15:28:50 +0000

    Added OutInstruction

---
+++ src/sml/instruction/OutInstruction.java
@@ -0,0 +1,35 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+
+// TODO: write a JavaDoc for the class
+
+/**
+ * @author Pedro Bustamante Munguira
+ */
+
+public class OutInstruction extends Instruction {
+
+    private final RegisterName source;
+
+    public static final String OP_CODE = "out";
+
+    public OutInstruction(String label, RegisterName source) {
+        super(label, OP_CODE);
+        this.source = source;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        int value = m.getRegisters().get(source);
+        System.out.println(value);
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
+    }
+
+    @Override
+    public String toString() {
+        return getLabelString() + getOpcode() + " " + source;
+    }
+}
