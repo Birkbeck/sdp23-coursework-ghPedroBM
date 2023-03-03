dd6bb56166656c944a835f74505844806b06e08a
ghPedroBM <31487343+ghPedroBM@users.noreply.github.com>
2023-03-03 11:25:03 +0000

    Added unit tests for toString, equals, and hashCode methods of MovInstruction. Method toString fails

--- test/sml/instruction/MovInstructionTest.java
+++ test/sml/instruction/MovInstructionTest.java
@@ -43,4 +43,43 @@ class MovInstructionTest {
         instruction.execute(machine);
         Assertions.assertEquals(7, machine.getRegisters().get(EAX));
     }
+
+
+
+    @Test
+    void testToString() {
+        Instruction instruction = new MovInstruction(null, EAX, -1);
+        String expectedString = "mov EAX -1";
+        String actualString = instruction.toString();
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    void testEqualsTrue() {
+        Instruction instruction1 = new MovInstruction(null, EAX, 7);
+        Instruction instruction2 = new MovInstruction(null, EAX, 7);
+        Assertions.assertTrue(instruction1.equals(instruction2));
+        Assertions.assertTrue(instruction2.equals(instruction1));
+    }
+
+    @Test
+    void testEqualsFalse() {
+        Instruction instruction1 = new MovInstruction(null, EAX, 7);
+        Instruction instruction2 = new MovInstruction(null, EBX, -7);
+        Assertions.assertFalse(instruction1.equals(instruction2));
+        Assertions.assertFalse(instruction2.equals(instruction1));
+    }
+
+    @Test
+    void testHashCodeTrue() {
+        Instruction instruction1 = new MovInstruction(null, EAX, 11);
+        Instruction instruction2 = new MovInstruction(null, EAX, 11);
+        Assertions.assertTrue(instruction1.hashCode()==instruction2.hashCode());
+    }
+
+    void testHashCodeFalse() {
+        Instruction instruction1 = new MovInstruction(null, EAX, 11);
+        Instruction instruction2 = new MovInstruction(null, EBX, -11);
+        Assertions.assertFalse(instruction1.hashCode()==instruction2.hashCode());
+    }
 }
