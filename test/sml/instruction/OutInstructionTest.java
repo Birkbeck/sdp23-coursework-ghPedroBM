d91626dfb605140112a1b672476941853c0ba1b9
ghPedroBM <31487343+ghPedroBM@users.noreply.github.com>
2023-03-03 11:34:38 +0000

    Added unit tests for toString, equals, and hashCode methods of OutInstruction

--- test/sml/instruction/OutInstructionTest.java
+++ test/sml/instruction/OutInstructionTest.java
@@ -43,4 +43,47 @@ class OutInstructionTest {
         instruction.execute(machine);
         Assertions.assertEquals(-5, machine.getRegisters().get(EAX));
     }
+
+
+
+    @Test
+    void testToString() {
+        Instruction instruction = new OutInstruction(null, EAX);
+        String expectedString = "out EAX";
+        String actualString = instruction.toString();
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    void testEqualsTrue() {
+        Instruction instruction1 = new OutInstruction(null, EAX);
+        Instruction instruction2 = new OutInstruction(null, EAX);
+        Assertions.assertTrue(instruction1.equals(instruction2));
+        Assertions.assertTrue(instruction2.equals(instruction1));
+    }
+
+    @Test
+    void testEqualsFalse() {
+        Instruction instruction1 = new OutInstruction(null, EAX);
+        Instruction instruction2 = new OutInstruction(null, EBX);
+        Assertions.assertFalse(instruction1.equals(instruction2));
+        Assertions.assertFalse(instruction2.equals(instruction1));
+    }
+
+    @Test
+    void testHashCodeTrue() {
+        Instruction instruction1 = new OutInstruction(null, EAX);
+        Instruction instruction2 = new OutInstruction(null, EAX);
+        Assertions.assertTrue(instruction1.hashCode()==instruction2.hashCode());
+    }
+
+    void testHashCodeFalse() {
+        Instruction instruction1 = new OutInstruction(null, EAX);
+        Instruction instruction2 = new OutInstruction(null, EBX);
+        Assertions.assertFalse(instruction1.hashCode()==instruction2.hashCode());
+    }
+
+
+
+
 }
