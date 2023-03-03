0e7191066dd30515f0577e0abc5a976b8bb609f1
ghPedroBM <31487343+ghPedroBM@users.noreply.github.com>
2023-03-03 12:23:02 +0000

    More comprehensive unit tests for toString, equals, and hashCode methods in SubInstruction

--- test/sml/instruction/SubInstructionTest.java
+++ test/sml/instruction/SubInstructionTest.java
@@ -45,9 +45,10 @@ class SubInstructionTest {
         instruction.execute(machine);
         Assertions.assertEquals(-11, machine.getRegisters().get(EAX));
     }
-    
+
+    // Test toString
     @Test
-    void testToString() {
+    void testToStringNullLabel() {
         Instruction instruction = new SubInstruction(null, EAX, EBX);
         String expectedString = "sub EAX EBX";
         String actualString = instruction.toString();
@@ -55,7 +56,15 @@ class SubInstructionTest {
     }
 
     @Test
-    void testEqualsTrue() {
+    void testToStringNonNullLabel() {
+        Instruction instruction = new SubInstruction("l 1", EAX, EBX);
+        String expectedString = "l 1: sub EAX EBX";
+        String actualString = instruction.toString();
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    void testEqualsTrueNullLabel() {
         Instruction instruction1 = new SubInstruction(null, EAX, EBX);
         Instruction instruction2 = new SubInstruction(null, EAX, EBX);
         Assertions.assertTrue(instruction1.equals(instruction2));
@@ -63,7 +72,16 @@ class SubInstructionTest {
     }
 
     @Test
-    void testEqualsFalse() {
+    void testEqualsTrueNonNullLabel() {
+        Instruction instruction1 = new SubInstruction("l 1", EAX, EBX);
+        Instruction instruction2 = new SubInstruction("l 1", EAX, EBX);
+        Assertions.assertTrue(instruction1.equals(instruction2));
+        Assertions.assertTrue(instruction2.equals(instruction1));
+    }
+
+    // Test equals
+    @Test
+    void testEqualsFalseNullLabelDifferentRegisters() {
         Instruction instruction1 = new SubInstruction(null, EAX, EAX);
         Instruction instruction2 = new SubInstruction(null, EBX, EBX);
         Assertions.assertFalse(instruction1.equals(instruction2));
@@ -71,15 +89,46 @@ class SubInstructionTest {
     }
 
     @Test
-    void testHashCodeTrue() {
+    void testEqualsFalseNonNullLabelDifferentRegisters() {
+        Instruction instruction1 = new SubInstruction("l 1", EAX, EAX);
+        Instruction instruction2 = new SubInstruction("l 1", EBX, EBX);
+        Assertions.assertFalse(instruction1.equals(instruction2));
+        Assertions.assertFalse(instruction2.equals(instruction1));
+    }
+
+    @Test
+    void testEqualsFalseNonNullLabelDifferentLabels() {
+        Instruction instruction1 = new SubInstruction("l 1", EAX, EAX);
+        Instruction instruction2 = new SubInstruction("l1", EAX, EAX);
+        Assertions.assertFalse(instruction1.equals(instruction2));
+        Assertions.assertFalse(instruction2.equals(instruction1));
+    }
+
+    // Test hashCode
+    @Test
+    void testHashCodeTrueNullLabels() {
         Instruction instruction1 = new SubInstruction(null, EAX, EBX);
         Instruction instruction2 = new SubInstruction(null, EAX, EBX);
         Assertions.assertTrue(instruction1.hashCode()==instruction2.hashCode());
     }
 
-    void testHashCodeFalse() {
-        Instruction instruction1 = new SubInstruction(null, EAX, EAX);
-        Instruction instruction2 = new SubInstruction(null, EBX, EBX);
+    @Test
+    void testHashCodeTrueNonNullLabels() {
+        Instruction instruction1 = new SubInstruction("l 1", EAX, EBX);
+        Instruction instruction2 = new SubInstruction("l 1", EAX, EBX);
+        Assertions.assertTrue(instruction1.hashCode()==instruction2.hashCode());
+    }
+
+    @Test
+    void testHashCodeFalseNonNullLabelsDifferentRegisters() {
+        Instruction instruction1 = new SubInstruction("l 1", EAX, EAX);
+        Instruction instruction2 = new SubInstruction("l 1", EBX, EBX);
+        Assertions.assertFalse(instruction1.hashCode()==instruction2.hashCode());
+    }
+
+    void testHashCodeFalseNonNullLabelsDifferentLabels() {
+        Instruction instruction1 = new SubInstruction("l 1", EAX, EAX);
+        Instruction instruction2 = new SubInstruction("l1", EBX, EBX);
         Assertions.assertFalse(instruction1.hashCode()==instruction2.hashCode());
     }
 
