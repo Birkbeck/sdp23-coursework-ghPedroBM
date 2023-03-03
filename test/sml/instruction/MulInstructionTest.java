d1893d02526d1663582d588ac5521faf622a138f
ghPedroBM <31487343+ghPedroBM@users.noreply.github.com>
2023-03-03 12:56:35 +0000

    More comprehensive unit tests for toString, equals, and hashCode methods in MulInstruction

--- test/sml/instruction/MulInstructionTest.java
+++ test/sml/instruction/MulInstructionTest.java
@@ -46,8 +46,9 @@ class MulInstructionTest {
         Assertions.assertEquals(-30, machine.getRegisters().get(EAX));
     }
 
+    // Test toString
     @Test
-    void testToString() {
+    void testToStringNullLabel() {
         Instruction instruction = new MulInstruction(null, EAX, EBX);
         String expectedString = "mul EAX EBX";
         String actualString = instruction.toString();
@@ -55,7 +56,15 @@ class MulInstructionTest {
     }
 
     @Test
-    void testEqualsTrue() {
+    void testToStringNonNullLabel() {
+        Instruction instruction = new MulInstruction("l 1", EAX, EBX);
+        String expectedString = "l 1: mul EAX EBX";
+        String actualString = instruction.toString();
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    void testEqualsTrueNullLabel() {
         Instruction instruction1 = new MulInstruction(null, EAX, EBX);
         Instruction instruction2 = new MulInstruction(null, EAX, EBX);
         Assertions.assertTrue(instruction1.equals(instruction2));
@@ -63,7 +72,16 @@ class MulInstructionTest {
     }
 
     @Test
-    void testEqualsFalse() {
+    void testEqualsTrueNonNullLabel() {
+        Instruction instruction1 = new MulInstruction("l 1", EAX, EBX);
+        Instruction instruction2 = new MulInstruction("l 1", EAX, EBX);
+        Assertions.assertTrue(instruction1.equals(instruction2));
+        Assertions.assertTrue(instruction2.equals(instruction1));
+    }
+
+    // Test equals
+    @Test
+    void testEqualsFalseNullLabelDifferentRegisters() {
         Instruction instruction1 = new MulInstruction(null, EAX, EAX);
         Instruction instruction2 = new MulInstruction(null, EBX, EBX);
         Assertions.assertFalse(instruction1.equals(instruction2));
@@ -71,15 +89,46 @@ class MulInstructionTest {
     }
 
     @Test
-    void testHashCodeTrue() {
+    void testEqualsFalseNonNullLabelDifferentRegisters() {
+        Instruction instruction1 = new MulInstruction("l 1", EAX, EAX);
+        Instruction instruction2 = new MulInstruction("l 1", EBX, EBX);
+        Assertions.assertFalse(instruction1.equals(instruction2));
+        Assertions.assertFalse(instruction2.equals(instruction1));
+    }
+
+    @Test
+    void testEqualsFalseNonNullLabelDifferentLabels() {
+        Instruction instruction1 = new MulInstruction("l 1", EAX, EAX);
+        Instruction instruction2 = new MulInstruction("l1", EAX, EAX);
+        Assertions.assertFalse(instruction1.equals(instruction2));
+        Assertions.assertFalse(instruction2.equals(instruction1));
+    }
+
+    // Test hashCode
+    @Test
+    void testHashCodeTrueNullLabels() {
         Instruction instruction1 = new MulInstruction(null, EAX, EBX);
         Instruction instruction2 = new MulInstruction(null, EAX, EBX);
         Assertions.assertTrue(instruction1.hashCode()==instruction2.hashCode());
     }
 
-    void testHashCodeFalse() {
-        Instruction instruction1 = new MulInstruction(null, EAX, EAX);
-        Instruction instruction2 = new MulInstruction(null, EBX, EBX);
+    @Test
+    void testHashCodeTrueNonNullLabels() {
+        Instruction instruction1 = new MulInstruction("l 1", EAX, EBX);
+        Instruction instruction2 = new MulInstruction("l 1", EAX, EBX);
+        Assertions.assertTrue(instruction1.hashCode()==instruction2.hashCode());
+    }
+
+    @Test
+    void testHashCodeFalseNonNullLabelsDifferentRegisters() {
+        Instruction instruction1 = new MulInstruction("l 1", EAX, EAX);
+        Instruction instruction2 = new MulInstruction("l 1", EBX, EBX);
+        Assertions.assertFalse(instruction1.hashCode()==instruction2.hashCode());
+    }
+
+    void testHashCodeFalseNonNullLabelsDifferentLabels() {
+        Instruction instruction1 = new MulInstruction("l 1", EAX, EAX);
+        Instruction instruction2 = new MulInstruction("l1", EBX, EBX);
         Assertions.assertFalse(instruction1.hashCode()==instruction2.hashCode());
     }
 
